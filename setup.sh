452232e53338b04e77c6df805756c8e885bc5821
kwerklund <kwerklund@gettyimages.com>
2013-09-01 00:54:29 -0600

    making changes to setup files

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install software
+#sh get_software
+
+#FILES=~/.config/.??*
+#FILES=~/config/.??*
+#for f in $FILES
+#do
+#  echo "Processing $f file..."
+#  ln -s $f ../
+#  # take action on each file. $f store current file name
+#done
+
+# Get vim plugins
+sh get_vim_plugins
+
+#FILES=~/.config/*
+#for f in $FILES
+#do
+#  echo "Processing $f file..."
+#  # take action on each file. $f store current file name
+#  #cat $f
+#done
+
