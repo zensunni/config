31d98e7b585760b60cce54c85696ffd0e4226ea7
kirk <kirk.werklund@gmail.com>
2014-03-15 14:51:32 -0600

    Adds changes to setup.sh for OS detection.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# Detect the OS:
+case "$OSTYPE" in
+  solaris*) echo "Detected: SOLARIS"
+    OS=SOLARIS;;
+  darwin*)  echo "Detected: OSX"
+    OS=OSX;; 
+  linux*)   echo "Detected: LINUX"
+    OS=LINUX;; 
+  bsd*)     echo "Detected: BSD"
+    OS=BSD;; 
+  *)        echo "Unknown: $OSTYPE"
+    OS="Unknown: $OSTYPE";; 
+esac
+# Source: http://f00bar.com/
+
 # Install software
-#sh get_software
+if [[ OS == "LINUX" ]]; then
+  sh get_software
+fi
 
 # Link files
-sh link_files
+#sh link_files
 
 # Get vim plugins
 #sh get_vim_plugins
