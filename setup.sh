6a37f730f713faafe8e3c5ea32250d4b94164cc0
kirk <kirk.werklund@gmail.com>
2014-03-15 14:22:54 -0600

    Fixes setup and moves sym link setup into it's own file.

--- setup.sh
+++ setup.sh
@@ -3,15 +3,9 @@
 # Install software
 #sh get_software
 
-#FILES=~/.config/.??*
-FILES=~/config/.??*
-for f in $FILES
-do
-  echo "Processing $f file..."
-  ln -s $f ../
-  # take action on each file. $f store current file name
-done
+# Link files
+sh link_files
 
 # Get vim plugins
-sh get_vim_plugins
+#sh get_vim_plugins
 
