597206d6377be03b55bd9d2b18e06321921313e0
kwerklund <kwerklund@gettyimages.com>
2014-03-15 11:42:17 -0600

    Adds git ignore to vim color files. Updates setup script.

--- setup.sh
+++ setup.sh
@@ -4,22 +4,14 @@
 #sh get_software
 
 #FILES=~/.config/.??*
-#FILES=~/config/.??*
-#for f in $FILES
-#do
-#  echo "Processing $f file..."
-#  ln -s $f ../
-#  # take action on each file. $f store current file name
-#done
+FILES=~/config/.??*
+for f in $FILES
+do
+  echo "Processing $f file..."
+  ln -s $f ../
+  # take action on each file. $f store current file name
+done
 
 # Get vim plugins
 sh get_vim_plugins
-
-#FILES=~/.config/*
-#for f in $FILES
-#do
-#  echo "Processing $f file..."
-#  # take action on each file. $f store current file name
-#  #cat $f
-#done
 
