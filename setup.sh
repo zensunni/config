b2fa2badadc73e49147df24aa7c4d94452b299bb
kirk <kirk.werklund@gmail.com>
2014-03-15 19:01:56 -0600

    Refactored setup script.

--- setup.sh
+++ setup.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 # Detect the OS:
-case "$OSTYPE" in
-  solaris*) echo "Detected: SOLARIS"
-    OS=SOLARIS;;
-  darwin*)  echo "Detected: OSX"
-    OS=OSX;; 
-  linux*)   echo "Detected: LINUX"
-    OS=LINUX;; 
-  bsd*)     echo "Detected: BSD"
-    OS=BSD;; 
-  *)        echo "Unknown: $OSTYPE"
-    OS="Unknown: $OSTYPE";; 
+case $OSTYPE in
+  solaris*) OS=SOLARIS;;
+  darwin*)  OS=OSX;; 
+  linux*)   OS=LINUX;; 
+  bsd*)     OS=BSD;; 
+  *)        OS="Unknown: $OSTYPE";; 
 esac
 # Source: http://f00bar.com/
 
 # Install software
-if [[ OS == "LINUX" ]]; then
-  sh get_software
+if [[ $OS == "LINUX" ]]; then
+  apt-get install tmux vim
+elif [[ $OS == "OSX" ]]; then
+  # install homebrew
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" 
+  brew install tmux vim
 fi
 
 # Link files
-#sh link_files
+sh link_files
 
 # Get vim plugins
-#sh get_vim_plugins
+sh get_vim_plugins
 
