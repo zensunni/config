2ef858d93ec8e6f82c62a31e0deed1c14fedffda
kirk <kirk.werklund@gmail.com>
2014-03-16 15:07:19 -0600

    Adding installation script to run on brand new computer.

---
+++ install.sh
@@ -0,0 +1,13 @@
+# This script should be able to install everything
+
+# Step 1: Install git
+#sudo apt-get install git
+
+# Step 2: Create key
+echo "Go to this webpage to install keys: https://help.github.com/articles/generating-ssh-keys"
+exit 1
+
+# Step 3: clone repository
+#git clone git@github.com:zensunni/config.git
+
+
